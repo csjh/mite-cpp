6c1fc0a389122cf5f1170f92eddcd57723e8b9c1
csjh <61282104+csjh@users.noreply.github.com>
2024-06-12 10:48:13 -0400

    clean up more

--- src/builtins/functions.cpp
+++ src/builtins/functions.cpp
@@ -3,33 +3,32 @@
 BinaryenFunctionRef strcmp(BinaryenModuleRef mod) {
     auto u8_ = u8(mod);
     auto u32_ = u32(mod);
-    auto v128_ = v128(mod);
-    auto i8x16_ = i8x16(mod);
+    auto u8x16_ = i8x16(mod);
 
-    auto STRING_1 = Local<0>(mod, &u32_);
-    auto STRING_2 = Local<1>(mod, &u32_);
-    auto LENGTH = Local<2>(mod, &u32_);
-    auto CTZ = Local<3>(mod, &u32_);
-    auto IF_ALL_ELSE = Local<4>(mod, &u32_);
+    auto s1 = Local<0>(mod, &u32_);
+    auto s2 = Local<1>(mod, &u32_);
+    auto len = Local<2>(mod, &u32_);
+    auto ctz = Local<3>(mod, &u32_);
+    auto tiebreaker = Local<4>(mod, &u32_);
 
     // in my defense, this is basically a DSL
 
-#define LOAD(ptr, offset, type)                                                \
+#define LOAD(type, ptr, offset)                                                \
     WasmExpression(mod, &type,                                                 \
                    BinaryenLoad(mod, type.size, false, offset, 0, type.t##ype, \
                                 ptr, "main_memory"))
 #define i32const(value)                                                        \
     WasmExpression(mod, &u32_, BinaryenConst(mod, BinaryenLiteralInt32(16)))
 #define i8x16const(value)                                                      \
-    WasmExpression(mod, &i8x16_,                                               \
+    WasmExpression(mod, &u8x16_,                                               \
                    BinaryenConst(mod, BinaryenLiteralVec128(value)))
 #define i8x16splat(value)                                                      \
-    WasmExpression(mod, &i8x16_,                                               \
+    WasmExpression(mod, &u8x16_,                                               \
                    BinaryenUnary(mod, BinaryenSplatVecI8x16(), value))
 #define i32ctz(value)                                                          \
     WasmExpression(mod, &u32_, BinaryenUnary(mod, BinaryenCtzInt32(), value))
 #define i8x16bitmask(value)                                                    \
-    WasmExpression(mod, &i8x16_,                                               \
+    WasmExpression(mod, &u8x16_,                                               \
                    BinaryenUnary(mod, BinaryenBitmaskVecI8x16(), value))
 #define SELECT(cond, true_, false_)                                            \
     WasmExpression(                                                            \
@@ -45,44 +44,38 @@ BinaryenFunctionRef strcmp(BinaryenModuleRef mod) {
                   NUMARGS(__VA_ARGS__), BinaryenTypeAuto())
 #define LOOP(name, body) BinaryenLoop(mod, name, body)
 #define RETURN(value) BinaryenReturn(mod, value)
-#define BREAK(name, condition, value) BinaryenBreak(mod, name, condition, value)
+#define BREAK_IF(name, condition) BinaryenBreak(mod, name, condition, NULL)
+
+    const char *loopname = "cmp_loop";
+    uint8_t mask[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 
     // clang-format off
     BinaryenExpressionRef block[] = {
-        IF_ALL_ELSE = *STRING_1 - *STRING_2,
-        LENGTH = SELECT(*STRING_1 < *STRING_2, *STRING_1, *STRING_2),
+        tiebreaker = *s1 - *s2,
+        len = SELECT(*s1 < *s2, *s1, *s2),
 
         IF(
-            LENGTH >= i32const(16),
+            len >= i32const(u8x16_.size),
             LOOP(
-                "cmp_loop",
+                loopname,
                 BLOCK(
                     IF(
-                        CTZ = i8x16bitmask(LOAD(STRING_1, 4, i8x16_) != LOAD(STRING_2, 4, i8x16_)),
-                        RETURN(LOAD(STRING_1 + (CTZ = i32ctz(CTZ)), 4, u8_) - LOAD(STRING_1 + CTZ, 4, u8_))),
+                        ctz = i8x16bitmask(LOAD(u8x16_, s1, u32_.size) != LOAD(u8x16_, s2, u32_.size)),
+                        RETURN(LOAD(u8_, s1 + (ctz = i32ctz(ctz)), u32_.size) - LOAD(u8_, s1 + ctz, u32_.size))),
 
-                    STRING_1 = STRING_1 + i32const(16),
-                    STRING_2 = STRING_2 + i32const(16),
+                    s1 += i32const(u8x16_.size),
+                    s2 += i32const(u8x16_.size),
 
-                    BREAK(
-                        "cmp_loop",
-                        (LENGTH = LENGTH - i32const(16)) >= i32const(16),
-                        NULL)))),
+                    BREAK_IF(loopname, (len -= i32const(u8x16_.size)) >= i32const(u8x16_.size))))),
 
-        CTZ = i8x16bitmask(
-            (
-                LOAD(STRING_1, 4, i8x16_) !=
-                LOAD(STRING_2, 4, i8x16_)
-            ) & (
-                i8x16const(
-                    ((const unsigned char[]){0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15})
-                ) < i8x16splat(LENGTH))),
+        ctz = i8x16bitmask(
+            LOAD(u8x16_, s1, u32_.size) != LOAD(u8x16_, s2, u32_.size)
+                   & (i8x16const(mask) < i8x16splat(len))),
 
         SELECT(
-            CTZ, 
-            LOAD(STRING_1 + (LENGTH = i32ctz(CTZ)), 4, u8_) 
-                  - LOAD(STRING_2 + LENGTH, 4, u8_), 
-            IF_ALL_ELSE)
+            ctz, 
+            LOAD(u8_, s1 + (len = i32ctz(ctz)), u32_.size) - LOAD(u8_, s2 + len, u32_.size), 
+            tiebreaker)
     };
 
 #undef LOAD
