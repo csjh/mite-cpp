9cb0903961b39d2abcc4d789df70e1734a2e8484
csjh <61282104+csjh@users.noreply.github.com>
2024-06-15 23:55:23 -0400

    mess around a bit

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,40 @@
 #include <stdio.h>
 
 int main() {
-    BinaryenModuleRef module = BinaryenModuleCreate();
+    auto foomod =
+        BinaryenModuleParse("(module"
+                            "(type $0 (func (param i32) (result i32)))"
+                            "(export \"foo\" (func $foo))"
+                            "(func $foo (param $0 i32) (result i32)"
+                            "(i32.const 43)"
+                            ")"
+                            ")");
+    auto foo = BinaryenGetFunction(foomod, "foo");
+
+    auto module = BinaryenModuleCreate();
     BinaryenSetMemory(module, 0, 256, "main_memory", {}, {}, {}, {}, {}, 0,
                       false, false, "main_memory");
     BinaryenModuleSetFeatures(module, BinaryenFeatureAll());
-    BinaryenAddFunction(module, "foo", BinaryenTypeInt32(), BinaryenTypeInt32(),
-                        NULL, 0,
-                        BinaryenConst(module, BinaryenLiteralInt32(42)));
+    // BinaryenAddFunction(module, "foo", BinaryenTypeInt32(),
+    // BinaryenTypeInt32(),
+    //                     NULL, 0,
+    //                     BinaryenConst(module, BinaryenLiteralInt32(42)));
+
+    auto num_locals = BinaryenFunctionGetNumVars(foo);
+    auto *local_types = new BinaryenType[num_locals];
+    for (size_t i = 0; i < num_locals; i++) {
+        local_types[i] = BinaryenFunctionGetVar(foo, i);
+    }
+    BinaryenAddFunction(
+        module,
+        BinaryenFunctionGetName(foo),
+        BinaryenFunctionGetParams(foo),
+        BinaryenFunctionGetResults(foo),
+        local_types,
+        num_locals,
+        BinaryenFunctionGetBody(foo)
+    );
+
     BinaryenAddFunctionExport(module, "foo", "foo");
     (void)strcmp(module);
     BinaryenAddFunctionExport(module, "strcmp", "strcmp");
